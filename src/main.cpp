1b0f4c0489c500e10b925e735273daf7400acfb6
DiverJim84 <Jim@example.com>
2023-12-09 19:19:00 -0600

    Pinout of motor shield

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,27 @@
 #include <Arduino.h>
 
 
-//shaun test 3
-//Jim Test 2
+//MotorShieldPins()
+
+//Pin map
+unsigned char _M1EN = 2;
+unsigned char _M1DIR = 7;
+unsigned char _M1PWM = 9;
+unsigned char _M1DIAG = 6;
+unsigned char _M1OCM = A0;
+
+unsigned char _M2EN = 4;
+unsigned char _M2DIR = 8;
+unsigned char _M2PWM = 10;
+unsigned char _M2DIAG = 12;
+unsigned char _M2OCM = A1;
+
+bool _flipM1 = false;
+bool _flipM2 = false;
+
+
+
+
 void setup() {
 }
 
